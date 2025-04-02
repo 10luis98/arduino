170c0703fb2b989d7f6632b976f9f70099936dc6
greenenergyperuwifi <greenenergyperuwifi@gmail.com>
2025-04-01 21:55:55 -0500

    1

--- sietemodular/deteccion.cpp
+++ sietemodular/deteccion.cpp
@@ -11,33 +11,47 @@ volatile int bobina = 0;
 
 
 void detectarDiente() {
-    flancoDetectado = true;
-    unsigned long tiempoActual = micros();
-    tiempoEntreDientes = tiempoActual - tiempoAnterior;
-    tiempoAnterior = tiempoActual;
-    
-    // Si la diferencia de tiempos es grande, se detecta el diente de referencia
-    if (tiempoAnteriorDiente > tiempoEntreDientes * 2) {
-        cilindro = 0;
-        dienteReferenciaDetectado = true;
-    } else {
-        if (dienteReferenciaDetectado) {
-            cilindro++;
-            if (cilindro > 6) {
-                cilindro = 0;
-                dienteReferenciaDetectado = false;
-            }
-            if (cilindro == 1) tiempoEntreDientes += tiempoAnteriorDiente;
-            if (cilindro == 3) {
-              bobina=0;
-                int avanceFijo = 0;
-                float vance = 1.0 - (avanceFijo / 120.0);
-                long tiempoAjustado = (2 + vance) * tiempoEntreDientes - 2000;
-                if (tiempoAjustado < 0) tiempoAjustado = 0;
-                iniciarTimer5(tiempoAjustado);  // Ajuste de tiempo en base a detección
-            }
-        }
-    }
-    tiempoAnteriorDiente = tiempoEntreDientes;
-}
+  flancoDetectado = true;
+  unsigned long tiempoActual = micros();
+  tiempoEntreDientes = tiempoActual - tiempoAnterior;
+  tiempoAnterior = tiempoActual;
 
+  // Si la diferencia de tiempos es grande, se detecta el diente de referencia
+  if (tiempoAnteriorDiente > tiempoEntreDientes * 2) {
+    cilindro = 0;
+    dienteReferenciaDetectado = true;
+  } else {
+    if (dienteReferenciaDetectado) {
+      cilindro++;
+      if (cilindro > 6) {
+        cilindro = 0;
+        dienteReferenciaDetectado = false;
+      }
+      if (cilindro == 1) tiempoEntreDientes += tiempoAnteriorDiente;
+
+              bobina = cilindro-1;
+        int avanceFijo = 0;
+        float vance = 1.0 - (avanceFijo / 120.0);
+        long tiempoAjustado = (0 + vance) * tiempoEntreDientes - 2000;
+        if (tiempoAjustado < 0) tiempoAjustado = 0;
+        iniciarTimer5(tiempoAjustado);  // Ajuste de tiempo en base a detección
+      // if (cilindro == 1) {
+      //   bobina = 0;
+      //   int avanceFijo = 0;
+      //   float vance = 1.0 - (avanceFijo / 120.0);
+      //   long tiempoAjustado = (0 + vance) * tiempoEntreDientes - 2000;
+      //   if (tiempoAjustado < 0) tiempoAjustado = 0;
+      //   iniciarTimer5(tiempoAjustado);  // Ajuste de tiempo en base a detección
+      // }
+      // if (cilindro == 2) {
+      //   bobina = 1;
+      //   int avanceFijo = 0;
+      //   float vance = 1.0 - (avanceFijo / 120.0);
+      //   long tiempoAjustado = (0 + vance) * tiempoEntreDientes - 2000;
+      //   if (tiempoAjustado < 0) tiempoAjustado = 0;
+      //   iniciarTimer5(tiempoAjustado);  // Ajuste de tiempo en base a detección
+      // }
+    }
+  }
+  tiempoAnteriorDiente = tiempoEntreDientes;
+}
